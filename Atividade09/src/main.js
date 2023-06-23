f8ddfac2d1007ba4a0269b5aa0b58eab652211d2
Argel <argelhr@hotmail.com>
2023-06-23 09:51:38 -0300

    feat:sons

--- Atividade09/src/main.js
+++ Atividade09/src/main.js
@@ -8,6 +8,13 @@ import { createSkyBox } from './skybox';
 
 let chao = true
 let gravidade = -2
+let theme = await loadAudio('audio/theme.mp3')
+let quac = await loadAudio('audio/quack.mp3')
+let walk = await loadAudio('audio/walk.mp3')
+let wolf = await loadAudio('audio/wolf.mp3')
+let passaro = await loadAudio('audio/bemtivi.mp3')
+let velocidade = 1
+
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
@@ -35,20 +42,20 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const light = new THREE.AmbientLight(0x999999, 1);
 scene.add(light);
 
-const plight = new THREE.PointLight('0xffff00', 500, 500);
-let x = 200
-let y = 400
-let z = -10;
-plight.position.set(x, y, z);
-scene.add(plight);
+// const plight = new THREE.PointLight('0x111111', 500, 500);
+// let x = 200
+// let y = 400
+// let z = -10;
+// plight.position.set(x, y, z);
+// scene.add(plight);
 
-const sphere_geomatry = new THREE.SphereGeometry(1, 50, 32)
-const sphereColor = new THREE.MeshBasicMaterial({ color: 0xffffff })
-const sphere = new THREE.Mesh(sphere_geomatry, sphereColor)
-sphere.position.set(x, y, z)
-scene.add(sphere)
+// const sphere_geomatry = new THREE.SphereGeometry(1, 50, 32)
+// const sphereColor = new THREE.MeshBasicMaterial({ color: 0xffffff })
+// const sphere = new THREE.Mesh(sphere_geomatry, sphereColor)
+// sphere.position.set(x, y, z)
+// scene.add(sphere)
 
-//criacao do skybox
+//criacao da floresta
 const florestBox = await createSkyBox('florestBox', 400)
 florestBox.position.y = 200
 scene.add(florestBox)
@@ -80,9 +87,12 @@ keyDownUp(window)
 animate()
 
 function animate() {
-	controls.update();
-	renderer.render(scene, camera)
-	move(patuve)
+
+
+	controls.update();//orbit
+	move(patuve)//movimentos do pato
+	sons()
+	renderer.render(scene, camera)//recria a cena
 	requestAnimationFrame(animate)
 }
 
@@ -92,33 +102,30 @@ window.addEventListener('click', evento => {
 
 function move(patao) {
 
-
-	let pz = patao.rotation.z
-
 	patao.rotation.z += hasKey("KeyD") ? -.1 : 0
 	patao.rotation.z += hasKey("KeyA") ? .1 : 0
 	console.log("x: " + patao.position.x)
 	console.log("y: " + patao.position.y)
 	console.log("z: " + patao.position.z)
 
-	let moveX = 0
-	let moveY = 0
+	// let moveX = 0
+	// let moveY = 0
 
 	// if (hasKey('KeyW')) {
 	// 	moveX = Math.cos(pz)
 	// 	moveY = Math.sin(pz)
 	// }
-	if (hasKey('KeyS')) {
-		moveX = -Math.cos(pz)
-		moveY = -Math.sin(pz)
-	}
+	// if (hasKey('KeyS')) {
+	// 	moveX = -Math.cos(pz)
+	// 	moveY = -Math.sin(pz)
+	// }
 
-	patao.position.z += hasKey('KeyW') ? Math.cos(pz) : 0
-	patao.position.x += hasKey('KeyW') ? Math.sin(pz) : 0
-	patao.position.z += hasKey('KeyS') ? -Math.cos(pz) : 0
-	patao.position.x += hasKey('KeyS') ? -Math.sin(pz) : 0
+	let pz = patao.rotation.z
 
-
+	patao.position.z += hasKey('KeyW') ? Math.cos(pz) * velocidade : 0
+	patao.position.x += hasKey('KeyW') ? Math.sin(pz) * velocidade : 0
+	patao.position.z += hasKey('KeyS') ? -Math.cos(pz) * velocidade : 0
+	patao.position.x += hasKey('KeyS') ? -Math.sin(pz) * velocidade : 0
 
 	// if (hasKey("Enter")) {
 	// 	if (Math.cos(pz) > 0) {
@@ -132,12 +139,20 @@ function move(patao) {
 	// 	}
 	// }
 
-	if (hasKey("Space") && chao && patao.position.y == 0) {//quando clica no espaço começa o pulo
+	if (hasKey("Enter")) {
+		if (quac.paused) {
+			quac.currenttime = 0
+			quac.play()
+		}
+	}
+
+	if (hasKey("Space") && chao && patao.position.y == 0) {
+		//espaço começa o pulo, apenas quando esta no chao e na posição do eixo y = 0 
 		chao = false
 		console.log("apertouuu")
 	}
 	else {
-		if (!chao) {
+		if (!chao) {//pulando
 
 			if (patao.position.y <= 25) {
 				patao.position.y -= gravidade
@@ -152,11 +167,39 @@ function move(patao) {
 			else
 				patao.position.y = 0
 
-		} //gravidade do jogo
+		}
 	}
-	if(patao.position.x > -50 && patao.position.x<250
+	if (patao.position.x > -50 && patao.position.x < 250
 		&& patao.position.z > 120 && patao.position.z < 250)
 		patao.position.y = -10
+}
+
+function sons() {
+	if (theme && theme.paused) {
+		theme.currenttime = 0
+		theme.play()
+		velocidade = 5
+	}
+
+	if (wolf && wolf.paused && Math.random() > 0.99997 && Math.random() > 0.5) {
+		wolf.currenttime = 0
+		wolf.play()
+		velocidade = 5
+	}
+	if (wolf.paused)
+		velocidade = 1
+
+	if (passaro && passaro.paused && Math.random() > 0.999) {
+		passaro.currenttime = 0
+		passaro.play()
+	}
+	if (hasKey("KeyD") || hasKey("KeyS") || hasKey("KeyA") || hasKey("KeyW")) {
+		walk.currenttime = 0
+		walk.play()
+	}
+	else {
+		walk.pause();
+	}
 }
 
 
